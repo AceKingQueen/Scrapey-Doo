be08e59fd4dcaaf6424acc0c38ff61d896e89aa7
Allison <allisonkingwilliams@gmail.com>
2018-07-01 22:48:21 -0400

    articles showing up on click of scrape button

---
+++ public/App.js
@@ -0,0 +1,14 @@
+
+// Grab the articles as a json
+$("#scrape-button").on("click", function() {
+
+$.getJSON("/articles", function(data) {
+    // For each one
+    for (var i = 0; i < data.length; i++) {
+      // Display the apropos information on the page
+      $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
+    }
+  });
+
+
+});
