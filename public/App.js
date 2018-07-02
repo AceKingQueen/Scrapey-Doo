d421f39b81eeb68fcea375a50bbcdc02514f1634
Allison <allisonkingwilliams@gmail.com>
2018-07-01 23:25:41 -0400

    scraped articles populate with notes and save button

--- public/App.js
+++ public/App.js
@@ -6,9 +6,16 @@ $.getJSON("/articles", function(data) {
     // For each one
     for (var i = 0; i < data.length; i++) {
       // Display the apropos information on the page
-      $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
+      $("#articles")
+        .append("<p data-id='" + data[i]._id 
+    + "'>" + data[i].title 
+    + "<br />" + data[i].link + "</p>"+ "<a class='waves-effect waves-light green btn left'>Notes</a>" + "<a class='waves-effect waves-light green lighten-2 btn left'>Save</a>" + "<br /><br/>"
+        );
+    
     }
   });
 
 
 });
+
+{/* <a class="waves-effect waves-light btn">button</a> */}
